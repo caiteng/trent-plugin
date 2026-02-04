359bce706af9c09038ed9ae33b9d8863eee0f0c5
caiteng <caiteng@xx>
2026-02-04 09:09:17 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("java")
+//    id("org.jetbrains.kotlin.jvm") version "1.9.24"
+    id("org.jetbrains.intellij") version "1.17.3"
+}
+
+group = "org.trent"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven {
+        url = uri("https://maven.aliyun.com/repository/public")
+    }
+}
+dependencies {
+    implementation("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
+    implementation("org.jsoup:jsoup:1.16.2")
+
+}
+intellij {
+    version.set("2024.1.4")
+    type.set("IU")
+
+    plugins.set(listOf("com.intellij.java"))
+}
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+//    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+//        kotlinOptions.jvmTarget = "11"
+//    }
+
+    patchPluginXml {
+        sinceBuild.set("211")
+        untilBuild.set("9999.*")
+    }
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
